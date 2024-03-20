69a15258e57e32b069e9dd023922d2032c8075a5
lion_claw <lion_claw@163.com>
2024-03-21 00:08:43 +0800

    不再管理生命周期了，目前实现了主体功能。

--- src/note-window.c
+++ src/note-window.c
@@ -81,10 +81,8 @@ note_window_init(NoteWindow *self)
 	// self->header_bar = ADW_HEADER_BAR(adw_header_bar_new());
 	// self->page=ADW_TAB_VIEW(adw_tab_view_new());
 	GtkWidget *list_box = gtk_list_box_new();
-	Note *note = g_object_new(NOTE_TYPE_NOTE, NULL);
 	AdwTabPage *list = adw_tab_view_append(self->page, GTK_WIDGET(list_box));
-	filename = g_strconcat(g_get_home_dir(), "/Documents/file.txt", NULL);
-	JsonParser* parser = json_parser_new();
+	filename = g_strconcat(g_get_home_dir(), "/.local/share/Note/data.json", NULL);
 	// strcat(filename, "/Documents/file.txt"); // 代码会导致不能自动释放资源。
 	GFile *file = g_file_new_for_path(filename);
 	if (g_file_query_exists(file, NULL) != true)
@@ -94,34 +92,102 @@ note_window_init(NoteWindow *self)
 	// g_file_set_contents(filename, "", -1, NULL);
 	// }
 	g_object_unref(file);
+	JsonParser* parser = json_parser_new();
+	json_parser_load_from_file(parser, filename, NULL);
 	// GFileIOStream *fd;
 	// if((fd=g_file_open_readwrite(filename, NULL, NULL)==G_IO_ERROR_NOT_FOUND)){
 	// 	g_file_create(file, G_FILE_CREATE_PRIVATE, NULL, NULL);
 	// 	g_file_new_for_path()
 	// }
 	// g_free(filename);
-	gtk_label_set_label(note->date, "2024/3/20");
+	JsonReader *reader = json_reader_new(json_parser_get_root(parser));
+	json_reader_read_member(reader, "timestamp");
+	int num = json_reader_count_elements(reader);
+	// reader = json_reader_new(json_parser_get_root(parser));
+	g_object_unref(reader);
+	// g_free(parser);
+	// JsonParser* parser = json_parser_new();
+	// json_parser_load_from_file(parser, filename, NULL);
+	JsonNode* root = json_parser_get_root(parser);
+	JsonObject* root_obj = json_node_dup_object(root);
+	JsonNode* dt_node = json_object_get_member(root_obj, "timestamp");
+	JsonArray* dt = json_node_get_array(dt_node);
+	JsonNode* cont_node = json_object_get_member(root_obj, "content");
+	JsonArray* cont = json_node_get_array(cont_node);
+
+	printf("%d\n", num);
+	for(int i=0;i<num;i++){
+		Note *note = g_object_new(NOTE_TYPE_NOTE, NULL);
+
+		// json_reader_read_member(reader, "timestamp");
+		// json_reader_read_element(reader, i);
+		// char* date = json_reader_get_string_value(reader);
+		// json_reader_end_element(reader);
+		// json_reader_end_member(reader);
+		
+		// json_reader_read_member(reader, "content");
+		// json_reader_read_element(reader, i);
+		// char* content = json_reader_get_string_value(reader);
+		// json_reader_end_element(reader);
+		// json_reader_end_member(reader);
+		
+		// g_object_unref (reader);
+		// g_object_unref (parser);
+		char* date = json_array_get_string_element(dt, i);
+		char* content = json_array_get_string_element(cont, i);
+
+		GtkWidget* image;
+		GDateTime* datetime = g_date_time_new_from_iso8601(date, NULL);
+		if(g_date_time_difference(g_date_time_new_now_local(), datetime)/G_TIME_SPAN_HOUR > 6){
+			// adw_action_row_set_icon_name(note->title,"changes-prevent-symbolic");
+			image = gtk_image_new_from_icon_name("changes-prevent-symbolic");
+		}else{
+			// adw_action_row_set_icon_name(note->title,"changes-allow-symbolic");
+			image = gtk_image_new_from_icon_name("changes-allow-symbolic");
+		}
+		char title[11];
+		char subtitle[31];
+		strncpy(title, content, 10);
+		adw_action_row_add_prefix(note->title,image);
+		adw_preferences_row_set_title(ADW_PREFERENCES_ROW(note->title), title);
+		strncpy(subtitle, content, 30);
+		adw_action_row_set_subtitle(note->title, subtitle);
+		adw_action_row_set_subtitle_lines(note->title, 2);
+		gtk_label_set_label(note->date, g_date_time_format(datetime, "%F %T"));
+		gtk_list_box_append(GTK_LIST_BOX(list_box), GTK_WIDGET(note));
+		g_free(datetime);
+	}
+	// gtk_label_set_label(note->date, "2024/3/20");
 	// note->title = ADW_ACTION_ROW(adw_action_row_new());
-	gtk_list_box_append(GTK_LIST_BOX(list_box), GTK_WIDGET(note));
+	// gtk_list_box_append(GTK_LIST_BOX(list_box), GTK_WIDGET(note));
 	adw_tab_view_set_selected_page(self->page, list);
 	// gtk_widget_show(GTK_WIDGET(self->page));
+	// json_array_unref(cont);
+	// json_array_unref(dt);
+	// json_node_unref(dt_node);
+	// json_node_unref(cont_node);
+	// json_object_unref(root_obj);
+	// json_node_unref(root);
+	// g_object_unref(parser);
 }
 
 void note_add_init(NoteWindow *self)
 {
+	// 这个函数只用来新建。
+
 	/* gtk_widget_set_visible (GTK_WIDGET (self->header_bar), (gboolean) FALSE); */
 	/* free (self->note_list); */
 	// GtkTextView * text = gtk_text_view_new_with_buffer(gtk_text_buffer_new(gtk_text_tag_table_new()))
-	GtkTextView *text;
-	GtkTextBuffer *buffer = gtk_text_buffer_new(NULL);
-	gchar *content;
-	g_file_get_contents(filename, &content, NULL, NULL);
-	gtk_text_buffer_set_text(buffer, content, -1);
-	g_free(content);
-	text = GTK_TEXT_VIEW(gtk_text_view_new_with_buffer(buffer));
+	// GtkTextView *text;
+	// GtkTextBuffer *buffer = gtk_text_buffer_new(NULL);
+	// gchar *content;
+	// g_file_get_contents(filename, &content, NULL, NULL);
+	// gtk_text_buffer_set_text(buffer, content, -1);
+	// g_free(content);
+	// text = GTK_TEXT_VIEW(gtk_text_view_new_with_buffer(buffer));
 
-	AdwTabPage *add = adw_tab_view_append(self->page, GTK_WIDGET(text));
-	// AdwTabPage *add = adw_tab_view_append(self->page, GTK_WIDGET(gtk_text_view_new()));
+	// AdwTabPage *add = adw_tab_view_append(self->page, GTK_WIDGET(text));
+	AdwTabPage *add = adw_tab_view_append(self->page, GTK_WIDGET(gtk_text_view_new()));
 	adw_tab_view_set_selected_page(self->page, add);
 	gtk_button_set_icon_name(self->add_page, "go-previous-symbolic");
 	// GtkListBox *list_box = GTK_LIST_BOX(gtk_stack_page_get_child(self->note_list));
@@ -131,8 +197,65 @@ void note_add_init(NoteWindow *self)
 	// printf("I am here\n");
 }
 
+static void json_add(const char* content){
+	
+	JsonParser* parser = json_parser_new();
+	json_parser_load_from_file(parser, filename, NULL);
+	JsonNode* root = json_parser_get_root(parser);
+
+	// JsonBuilder *builder = json_builder_new();
+	JsonGenerator *gen = json_generator_new();
+	// JsonNode* root = json_builder_get_root(builder);
+	// JsonReader *reader = json_reader_new(root);
+	JsonObject* root_obj = json_node_dup_object(root);
+	JsonNode* dt_node = json_object_get_member(root_obj, "timestamp");
+	JsonArray* dt = json_node_get_array(dt_node);
+	JsonNode* cont_node = json_object_get_member(root_obj, "content");
+	JsonArray* cont = json_node_get_array(cont_node);
+	
+	// json_builder_begin_object(builder);
+
+	// json_builder_set_member_name(builder, "timestamp");
+	// json_builder_begin_array(builder);
+	// json_builder_add_string_value(builder, g_date_time_format_iso8601(g_date_time_new_now_local()));
+	// json_builder_end_array(builder);
+
+	// json_builder_set_member_name(builder, "content");
+	// json_builder_begin_array(builder);
+	// json_builder_add_string_value(builder, content);
+	// json_builder_end_array(builder);
+	
+	// json_builder_end_object(builder);
+
+	json_array_add_string_element(dt, g_date_time_format_iso8601(g_date_time_new_now_local()));
+	json_array_add_string_element(cont, content);
+
+	json_generator_set_root(gen, root);
+	gchar *str = json_generator_to_data(gen, NULL);
+	// printf("%s", filename);
+	// if(json_generator_to_file(gen, filename, NULL)){
+	if(g_file_set_contents(filename, str, -1, NULL)){
+		printf("Write to file.\n");
+	}else{
+		printf("Error, check the path\n");
+	}
+	// json_node_free(root);
+	// g_object_unref(gen);
+	// json_array_unref(cont);
+	// json_array_unref(dt);
+	// json_node_unref(dt_node);
+	// json_node_unref(cont_node);
+	// json_object_unref(root_obj);
+	// json_node_unref(root);
+	// g_object_unref(parser);
+	// g_object_unref(builder);
+}
+
+
 void note_add_close(NoteWindow *self)
 {
+	/*NOTE - json member: id, timestamp, content, edited_times...
+	*/
 	AdwTabPage *add = adw_tab_view_get_selected_page(self->page);
 	GtkTextView *text = GTK_TEXT_VIEW(adw_tab_page_get_child(add));
 	GtkTextBuffer *text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text));
@@ -142,8 +265,9 @@ void note_add_close(NoteWindow *self)
 	gtk_text_buffer_get_start_iter(text_buffer, &start);
 	gtk_text_buffer_get_end_iter(text_buffer, &end);
 	content = gtk_text_buffer_get_text(text_buffer, &start, &end, false);
-	g_file_set_contents(filename, content, -1, NULL);
+	json_add(content);
+	// g_file_set_contents(filename, content, -1, NULL);
 	adw_tab_view_close_page(self->page, add);
 	gtk_button_set_icon_name(self->add_page, "list-add-symbolic");
-	g_free(content);
+	g_free(content); // TODO: 这里可以做一个优化，不要每次离开就立刻将text清理掉，改成程序关闭时。
 }
